119a4c6a651e9fc7e313a79781cb3ab6d8358bc5
Gillette Aziabah <victorga@access89.com>
2024-09-12 15:34:56 +0000

    new achnges

--- src/functions/hellworld.ts
+++ src/functions/hellworld.ts
@@ -1,15 +1,23 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import {
+	app,
+	HttpRequest,
+	HttpResponseInit,
+	InvocationContext,
+} from '@azure/functions';
 
-export async function hellworld(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+export async function hellworld(
+	request: HttpRequest,
+	context: InvocationContext,
+): Promise<HttpResponseInit> {
+	context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+	const name = request.query.get('name') || (await request.text()) || 'world';
 
-    return { body: `Hello, ${name}!` };
-};
+	return { body: `Hello, ${name} comign soon!` };
+}
 
 app.http('hellworld', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: hellworld
+	methods: ['GET', 'POST'],
+	authLevel: 'anonymous',
+	handler: hellworld,
 });
